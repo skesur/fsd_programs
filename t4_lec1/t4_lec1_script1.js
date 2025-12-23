456745e0451a0a8698f883f5fafc12ff298f3b6c
Saumya Kesur <saumyakesur@gmail.com>
2025-12-23 10:40:56 +0530

    (t4/lec1)-(23/12/2025)

---
+++ t4_lec1/t4_lec1_script1.js
@@ -0,0 +1,44 @@
+document.write("saumya <br>")
+
+// var a1 = 10
+// function f1(){
+//     var b1 = 10
+//     console.log(a1,b1)
+// }
+// f1();
+// console.log(a1)
+
+// function f2(){
+//     var a2 = 20
+//     console.log(a2)
+// }
+// f2();
+// console.log(a2)
+
+// function f3(){
+//     var a3 = 30
+//     console.log(a3)
+//     var a3 = 35
+//     console.log(a3)
+// }
+// f3();
+
+// function f4(){
+//     var b=5
+//     if (b>4){
+//         let a=1
+//         console.log(a)
+//     }console.log(a)
+// }
+// f4()
+
+// function f5(){
+//     var x=10
+//     console.log(x)
+//     let x=20
+//     console.log(x)
+//     x=30
+//     console.log(x)
+// }
+// f5()
+
